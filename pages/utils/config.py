cc5c1c74d413bf3820438494ad0d415f495e6551
William Fernandes Dias <william.winchester1967@gmail.com>
2023-08-24 15:39:12 -0300

    feat: configurar localização do projeto

--- pages/utils/config.py
+++ pages/utils/config.py
@@ -1,8 +1,12 @@
+import locale
+
+
 def header(st, page_name: str):
     st.header(page_name)
 
 
 def set_page_settings(st, layout: str = "wide") -> None:
+    locale.setlocale(locale.LC_ALL, "pt_BR.UTF-8")
     st.set_page_config(
         page_title="Valuation Projetos",
         page_icon="📋",
